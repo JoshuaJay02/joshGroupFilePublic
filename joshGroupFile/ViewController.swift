ad0dc556ab6f96169bf5d632e0a36999565ba05a
Kelvin <043655@gmail.com>
2019-10-29 12:56:05 -0400

    sdfg

--- joshGroupFile/ViewController.swift
+++ joshGroupFile/ViewController.swift
@@ -10,10 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageImage: UIImageView!
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         //Riley's changes
         print("please help me")
+        print("Kelvin")
     }
 
 
